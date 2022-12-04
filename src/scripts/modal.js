f41f932e9f3806cfb7fc93281a32e055204ecade
unknown <contato.abnersouza@gmail.com>
2022-12-04 01:16:34 -0300

    1

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,1 +1,10 @@
-/* Desenvolva seu código aqui... */
+const botaoModal = document.querySelectorAll("[data-modal-control]") 
+
+for( let i=0; i < botaoModal.length; i++){
+    botaoModal[i].addEventListener("click", function(){
+        let valueDataModalControl = botaoModal[i].getAttribute("data-modal-control")
+        document.getElementById(valueDataModalControl).classList.toggle("show-modal")
+    })
+}
+
+
